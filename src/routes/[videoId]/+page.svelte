feb3a0b82be51d8d090509c5a0e965cffa61471f
ornel77 <ornella.tomboza@gmail.com>
2023-10-23 08:56:56 +0200

    starting the iframe 1:27:05

---
+++ src/routes/[videoId]/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	let videoId = '';
+</script>
+
+<iframe
+	frameborder="0"
+	class="w-full h-[480px]"
+	src={`https://www.youtube.com/embed/${videoId}`}
+	title="Youtube video player"
+	allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+	allowfullscreen
+/>
+
+<div class="max-w-[1150px] px-2 w-full mx-auto" />
